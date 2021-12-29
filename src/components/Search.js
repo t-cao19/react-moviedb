46722cf765309c9cf0acc3b975af13b4d5985fd5
Tony Cao <tony.cao1@outlook.com>
2021-12-28 19:12:46 -0500

    Search box implemented

---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Search() {
+  return (
+    <section className="searchbox-wrap">
+      <input
+        type="text"
+        placeholder="Search for a movie..."
+        className="searchbox"
+      ></input>
+    </section>
+  );
+}
+
+export default Search;
