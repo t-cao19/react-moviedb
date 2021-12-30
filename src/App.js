da1d581e2fa418100ecf7d1350b066a0acdb0229
Tony Cao <tony.cao1@outlook.com>
2021-12-30 14:05:28 -0500

    Added state for search bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./components/Search";
 
 function App() {
+  const [state, setState] = useState({
+    s: "", // Search query
+    results: [], // Result array
+    selected: {}, // Clicked on
+  });
+
+  const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=9c632899";
+
+  const handleInput = (e) => {
+    // Handle value when typing
+    let s = e.target.value;
+
+    setState((prevState) => {
+      // Get previous state and change search query to the s above
+      return { ...prevState, s: s };
+    });
+
+    console.log(state.s);
+  };
+
   return (
     <div className="App">
       <header>
         <h1>Movie Database</h1>
       </header>
       <main>
-        <Search></Search>
+        <Search handleInput={handleInput} />
       </main>
     </div>
   );
